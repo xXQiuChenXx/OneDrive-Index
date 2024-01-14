1279cdb1e295503cc315465027adcebf1e116120
xXQiuChenXx <lautaihong2004@gmail.com>
2024-01-14 19:21:42 +0800

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,9 +1,11 @@
 import { exchangeCode } from "@/utils/graphAPI";
+import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  if (!code) return Response.redirect("/auth", 301);
+  
+  if (!code) return redirect("/auth");
   const data = await exchangeCode(code);
 
   return Response.json(data);
