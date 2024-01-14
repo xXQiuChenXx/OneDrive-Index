70b77db3ccb67c36a9d1e56a1ae0cb21834240e1
xXQiuChenXx <lautaihong2004@gmail.com>
2024-01-14 19:47:31 +0800

    Update page.tsx

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -3,7 +3,7 @@ import config from "@/config/api.config";
 const Setup = () => {
   const query = new URLSearchParams({
     client_id: config.CLIENT_ID,
-    scope: config.SCOPES.join("%20"),
+    scope: config.SCOPES.join(" "),
     response_type: "code",
     redirect_uri: config.REDIRECT_URI,
   });
